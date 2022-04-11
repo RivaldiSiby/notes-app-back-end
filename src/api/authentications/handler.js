084f735646699865c667b9bc5fb8c66b74b1811f
Rivaldi Siby <rivaldisiby@gmail.com>
2022-04-12 03:30:24 +0800

    authentication implemented

---
+++ src/api/authentications/handler.js
@@ -0,0 +1,135 @@
+const ClientError = require("../../exceptions/ClientError");
+
+class AuthenticationsHandler {
+  constructor(authenticationsService, usersService, tokenManager, validator) {
+    this._authenticationsService = authenticationsService;
+    this._usersService = usersService;
+    this._tokenManager = tokenManager;
+    this._validator = validator;
+
+    this.postAuthenticationHandler = this.postAuthenticationHandler.bind(this);
+    this.putAuthenticationHandler = this.putAuthenticationHandler.bind(this);
+    this.deleteAuthenticationHandler =
+      this.deleteAuthenticationHandler.bind(this);
+  }
+
+  async postAuthenticationHandler(request, h) {
+    try {
+      // cek validasi
+      this._validator.validatePostAuthenticationPayload(request.payload);
+
+      //   cek username dan password
+      const { username, password } = request.payload;
+      const id = await this._usersService.verifyUserCredential(
+        username,
+        password
+      );
+
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+      const refreshToken = this._tokenManager.generateRefreshToken({ id });
+
+      await this._authenticationsService.addRefreshToken(refreshToken);
+
+      const response = h.response({
+        status: "success",
+        message: "Authentication berhasil ditambahkan",
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      return response;
+    }
+  }
+
+  async putAuthenticationHandler(request, h) {
+    try {
+      // validasi
+      this._validator.validatePutAuthenticationPayload(request.payload);
+      const { refreshToken } = request.payload;
+
+      // cek refresh token
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
+      // cek refresh token key
+      const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
+      // membuat accesstoken baru
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+      return {
+        status: "success",
+        message: "Access Token berhasil diperbarui",
+        data: {
+          accessToken,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      return response;
+    }
+  }
+
+  async deleteAuthenticationHandler(request, h) {
+    try {
+      this._validator.validateDeleteAuthenticationPayload(request.payload);
+      const { refreshToken } = request.payload;
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
+      //   hapus token
+      await this._authenticationsService.deleteRefreshToken(refreshToken);
+      //   kembalikan response
+      return {
+        status: "success",
+        message: "Refresh token berhasil dihapus",
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      return response;
+    }
+  }
+}
+
+module.exports = AuthenticationsHandler;
